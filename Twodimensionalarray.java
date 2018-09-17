a148051597bf6f7935a6e739c82c8e9d41317913
SakthiMaheswariK <43263252+SakthiMaheswariK@users.noreply.github.com>
2018-09-17 19:31:10 +0530

    Add files via upload

---
+++ Twodimensionalarray.java
@@ -0,0 +1,23 @@
+package Twodimensionalarray;
+
+public class Twodimensionalarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a[]= {{1,1,1},{2,2,2},{3,3,3}},b= {20};
+		System.out.println(b[0]);
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				a[i][j]=100;
+				System.out.println(a[i][j]);
+			}
+
+		}
+			
+		}
+}
+	
+
+
