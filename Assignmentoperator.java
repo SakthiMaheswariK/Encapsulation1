e7f1e6b925d2f577c113609121920dec9fa1641d
SakthiMaheswariK <43263252+SakthiMaheswariK@users.noreply.github.com>
2018-09-17 19:27:24 +0530

    Add files via upload

---
+++ Assignmentoperator.java
@@ -0,0 +1,15 @@
+package Assignmentoperator;
+
+public class Assignmentoperator {
+public static void main(String args[])
+{
+	int sum=0,i=100;
+	while(i !=0)
+	{
+		sum +=i;
+		i--;
+		
+	}
+	System.out.println("sum="+sum);
+}
+}
